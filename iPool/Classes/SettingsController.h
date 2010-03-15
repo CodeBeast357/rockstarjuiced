a56d684ed6c105c3fc0692e3e797c625dbc1604a
alexandre.girard.2 <alexandre.girard.2@0169c884-2b0a-11df-9d00-8333cd69f872>
2010-03-15 23:58:47 +0000

    ajout settings controller

---
+++ iPool/Classes/SettingsController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsController.h
+//  NavApp
+//
+//  Created by alex on 15/03/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Delegate.h"
+
+@interface SettingsController : UIViewController  {
+Delegate *mainDelegate;
+}
+
+@end
---
+++ iPool/Classes/SettingsController.m
@@ -0,0 +1,36 @@
+//
+//  SettingsController.m
+//  NavApp
+//
+//  Created by alex on 15/03/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SettingsController.h"
+
+
+@implementation SettingsController
+
+//Source: http://stackoverflow.com/questions/772182/iphone-uiviewcontroller-init-method-not-being-called
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        
+		mainDelegate = (Delegate *)[[UIApplication sharedApplication] delegate];
+		
+    }
+    return self;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
