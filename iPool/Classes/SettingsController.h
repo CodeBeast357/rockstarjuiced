25a49982ffb727d88c48e2e52c7415b6e15a2eba
francois.monarque <francois.monarque@0169c884-2b0a-11df-9d00-8333cd69f872>
2010-03-21 01:05:20 +0000

    la logique pour la vue Settings est termine a 95%. Je fais le reste Lundi.

--- iPool/Classes/SettingsController.h
+++ iPool/Classes/SettingsController.h
@@ -14,18 +14,92 @@
 	Delegate *mainDelegate;
 	Settings *settings;
 	
+	BOOL keyboardVisible;
+	
+	IBOutlet UIView *settingsView;
+	IBOutlet UIScrollView *scrollView;
+	
 	IBOutlet UITextField *txtFieldPoolName;
 	IBOutlet UITextField *txtFieldNumberOfPlayers;
 	IBOutlet UISwitch *switchShowAdvanced;
+	
+	IBOutlet UITextField* txtFieldNumberOfFwd;
+	IBOutlet UITextField* txtFieldPointPerGoalFwd;
+	IBOutlet UITextField* txtFieldPointPerAssistFwd;
+	IBOutlet UITextField* txtFieldPlusMinusFwd;
+	IBOutlet UITextField* txtFieldNumberOfDef;
+	IBOutlet UITextField* txtFieldPointPerGoalDef;
+	IBOutlet UITextField* txtFieldPointPerAssistDef;
+	IBOutlet UITextField* txtFieldPlusMinusDef;
+	IBOutlet UITextField* txtFieldNumberOfGoalie;
+	IBOutlet UITextField* txtFieldPointPerWin;
+	IBOutlet UITextField* txtFieldPointPerOT;
+	IBOutlet UITextField* txtFieldPointPerShoutout;
+	IBOutlet UITextField* txtFieldShoutoutPts;
+	IBOutlet UITextField* txtFieldPointPerPts;
+	IBOutlet UITextField* txtFieldProlongationPts;
+	
+	IBOutlet UILabel* lblNumberOfFwd;
+	IBOutlet UILabel* lblPointPerGoalFwd;
+	IBOutlet UILabel* lblPointPerAssistFwd;
+	IBOutlet UILabel* lblPlusMinusFwd;
+	IBOutlet UILabel* lblNumberOfDef;
+	IBOutlet UILabel* lblPointPerGoalDef;
+	IBOutlet UILabel* lblPointPerAssistDef;
+	IBOutlet UILabel* lblPlusMinusDef;
+	IBOutlet UILabel* lblNumberOfGoalie;
+	IBOutlet UILabel* lblPointPerWin;
+	IBOutlet UILabel* lblPointPerOT;
+	IBOutlet UILabel* lblPointPerShoutout;
+	IBOutlet UILabel* lblShoutoutPts;
+	IBOutlet UILabel* lblPointPerPts;
+	IBOutlet UILabel* lblProlongationPts;
 }
 
+@property (nonatomic, retain) UIView *settingsView;
+@property (nonatomic, retain) UIScrollView *scrollView;
 @property (nonatomic, retain) Settings *settings;
 @property (nonatomic, retain) UITextField *txtFieldPoolName;
 @property (nonatomic, retain) UITextField *txtFieldNumberOfPlayers;
 @property (nonatomic, retain) UISwitch *switchShowAdvanced;
+@property (nonatomic, retain) UITextField* txtFieldNumberOfFwd;
+@property (nonatomic, retain) UITextField* txtFieldPointPerGoalFwd;
+@property (nonatomic, retain) UITextField* txtFieldPointPerAssistFwd;
+@property (nonatomic, retain) UITextField* txtFieldPlusMinusFwd;
+@property (nonatomic, retain) UITextField* txtFieldNumberOfDef;
+@property (nonatomic, retain) UITextField* txtFieldPointPerGoalDef;
+@property (nonatomic, retain) UITextField* txtFieldPointPerAssistDef;
+@property (nonatomic, retain) UITextField* txtFieldPlusMinusDef;
+@property (nonatomic, retain) UITextField* txtFieldNumberOfGoalie;
+@property (nonatomic, retain) UITextField* txtFieldPointPerWin;
+@property (nonatomic, retain) UITextField* txtFieldPointPerOT;
+@property (nonatomic, retain) UITextField* txtFieldPointPerShoutout;
+@property (nonatomic, retain) UITextField* txtFieldShoutoutPts;
+@property (nonatomic, retain) UITextField* txtFieldPointPerPts;
+@property (nonatomic, retain) UITextField* txtFieldProlongationPts;
+@property (nonatomic, retain) UILabel* lblNumberOfFwd;
+@property (nonatomic, retain) UILabel* lblPointPerGoalFwd;
+@property (nonatomic, retain) UILabel* lblPointPerAssistFwd;
+@property (nonatomic, retain) UILabel* lblPlusMinusFwd;
+@property (nonatomic, retain) UILabel* lblNumberOfDef;
+@property (nonatomic, retain) UILabel* lblPointPerGoalDef;
+@property (nonatomic, retain) UILabel* lblPointPerAssistDef;
+@property (nonatomic, retain) UILabel* lblPlusMinusDef;
+@property (nonatomic, retain) UILabel* lblNumberOfGoalie;
+@property (nonatomic, retain) UILabel* lblPointPerWin;
+@property (nonatomic, retain) UILabel* lblPointPerOT;
+@property (nonatomic, retain) UILabel* lblPointPerShoutout;
+@property (nonatomic, retain) UILabel* lblShoutoutPts;
+@property (nonatomic, retain) UILabel* lblPointPerPts;
+@property (nonatomic, retain) UILabel* lblProlongationPts;
 
 - (IBAction) save: (id) sender;
 - (IBAction) cancel: (id) sender;
 - (IBAction) txtFieldDoneEditing: (id) sender;
+- (IBAction) showAdvancedSettings: (id) sender;
+
+- (void)keyboardDidShow: (NSNotification*) notif;
+- (void)keyboardDidHide: (NSNotification*) notif;
+- (void)initTxtFields;
 
 @end
--- iPool/Classes/SettingsController.m
+++ iPool/Classes/SettingsController.m
@@ -9,8 +9,46 @@
 #import "SettingsController.h"
 
 @implementation SettingsController
+@synthesize scrollView;
 @synthesize settings;
-@synthesize txtFieldPoolName,txtFieldNumberOfPlayers,switchShowAdvanced;
+@synthesize switchShowAdvanced;
+@synthesize 
+ settingsView,
+ scrollView,
+ settings,
+ txtFieldPoolName,
+ txtFieldNumberOfPlayers,
+ switchShowAdvanced,
+ txtFieldNumberOfFwd,
+ txtFieldPointPerGoalFwd,
+ txtFieldPointPerAssistFwd,
+ txtFieldPlusMinusFwd,
+ txtFieldNumberOfDef,
+ txtFieldPointPerGoalDef,
+ txtFieldPointPerAssistDef,
+ txtFieldPlusMinusDef,
+ txtFieldNumberOfGoalie,
+ txtFieldPointPerWin,
+ txtFieldPointPerOT,
+ txtFieldPointPerShoutout,
+ txtFieldShoutoutPts,
+ txtFieldPointPerPts,
+ txtFieldProlongationPts,
+ lblNumberOfFwd,
+ lblPointPerGoalFwd,
+ lblPointPerAssistFwd,
+ lblPlusMinusFwd,
+ lblNumberOfDef,
+ lblPointPerGoalDef,
+ lblPointPerAssistDef,
+ lblPlusMinusDef,
+ lblNumberOfGoalie,
+ lblPointPerWin,
+ lblPointPerOT,
+ lblPointPerShoutout,
+ lblShoutoutPts,
+ lblPointPerPts,
+lblProlongationPts;
 
 //Source: http://stackoverflow.com/questions/772182/iphone-uiviewcontroller-init-method-not-being-called
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -27,6 +65,9 @@
 	
 	self.title = @"Pool Settings";
 	
+	scrollView.contentSize = CGSizeMake(320,1300);
+	switchShowAdvanced = NO;
+	
 	//Création des boutons Save et Cancel
 	self.navigationItem.leftBarButtonItem = [[[UIBarButtonItem alloc]  
 											 initWithBarButtonSystemItem:UIBarButtonSystemItemCancel  
@@ -40,10 +81,7 @@
 	settings = [Settings getInstance];
 	
 	//Initialize text fields
-	txtFieldPoolName.text = [settings poolName];
-	
-	if([settings numberOfPlayers] != 0)
-		txtFieldNumberOfPlayers.text = [NSString stringWithFormat:@"%d",[settings numberOfPlayers]];
+	[self initTxtFields];
 	
 }
 
@@ -62,6 +100,16 @@
 	
 	[settings setPoolName:txtFieldPoolName.text];
 	[settings setNumberOfPlayers:[txtFieldNumberOfPlayers.text intValue]];
+	[settings setOffencePlayerTotal:[txtFieldNumberOfFwd.text intValue]];
+	[settings setOffencePointsPerGoal:[txtFieldPointPerGoalFwd.text intValue]];
+	[settings setOffencePointsPerAssist:[txtFieldPointPerAssistFwd.text intValue]];
+	[settings setOffenceDifferentialActivated:[txtFieldPlusMinusFwd.text intValue]];
+	[settings setDefencePlayerTotal:[txtFieldNumberOfDef.text intValue]];
+	[settings setDefencePointsPerGoal:[txtFieldPointPerGoalDef.text intValue]];
+	[settings setDefencePointsPerAssist:[txtFieldPointPerAssistDef.text intValue]];
+	[settings setDefenceDifferentialActivated:[txtFieldPlusMinusDef.text intValue]];
+	
+	
 	[self dismissModalViewControllerAnimated:YES];
 }
 
@@ -72,7 +120,225 @@
 
 - (IBAction) txtFieldDoneEditing: (id) sender{
 	
+	if([txtFieldNumberOfFwd.text intValue] > 0){
+		
+		txtFieldPointPerGoalFwd.enabled = YES;
+		txtFieldPointPerAssistFwd.enabled = YES;
+		txtFieldPlusMinusFwd.enabled = YES;
+	}
+	else{
+		
+		txtFieldPointPerGoalFwd.enabled = NO;
+		txtFieldPointPerAssistFwd.enabled = NO;
+		txtFieldPlusMinusFwd.enabled = NO;
+		
+		txtFieldPointPerGoalFwd.text = @"";
+		txtFieldPointPerAssistFwd.text = @"";
+		txtFieldPlusMinusFwd.text = @"";
+	}
+	
+	if([txtFieldNumberOfDef.text intValue] > 0){
+		
+		txtFieldPointPerGoalDef.enabled = YES;
+		txtFieldPointPerAssistDef.enabled = YES;
+		txtFieldPlusMinusDef.enabled = YES;
+	}
+	else{
+		
+		txtFieldPointPerGoalDef.enabled = NO;
+		txtFieldPointPerAssistDef.enabled = NO;
+		txtFieldPlusMinusDef.enabled = NO;
+		
+		txtFieldPointPerGoalDef.text = @"";
+		txtFieldPointPerAssistDef.text = @"";
+		txtFieldPlusMinusDef.text = @"";
+	}
+	
+	if([txtFieldNumberOfGoalie.text intValue] > 0){
+		
+		txtFieldPointPerWin.enabled = YES;
+		txtFieldPointPerOT.enabled = YES;
+		txtFieldPointPerShoutout.enabled = YES;
+	}
+	else{
+		
+		txtFieldPointPerWin.enabled = NO;
+		txtFieldPointPerOT.enabled = NO;
+		txtFieldPointPerShoutout.enabled = NO;
+		
+		txtFieldPointPerWin.text = @"";
+		txtFieldPointPerOT.text = @"";
+		txtFieldPointPerShoutout.text = @"";
+	}
+	
+	
 	[sender resignFirstResponder];
+}
+
+/*----The fallowing methods are used to resize the scroll view when the keyboard show up---*/
+
+/*
+ *Before the view appear, we register events for
+ *when the keyboard will show.
+ */
+- (void) viewWillAppear: (BOOL)animated {
+	
+	[super viewWillAppear:animated];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:)
+		name:UIKeyboardDidShowNotification object:nil];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:)
+		name:UIKeyboardDidHideNotification object:nil];
+	
+	keyboardVisible = NO;
+}
+
+/*
+*Before the view disappear, we unregister events for
+*when the keyboard.
+*/
+- (void) keyboardViewWilldisappear: (BOOL)animated { 
+	
+	//Will remove every observers we have
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void) keyboardDidShow: (NSNotification*) notif{
+	
+	if(keyboardVisible == NO)
+	{
+		NSDictionary* info = [notif userInfo];
+		
+		NSValue* aValue = [info objectForKey:UIKeyboardBoundsUserInfoKey];  
+						   
+		CGSize keyboardSize = [aValue CGRectValue].size; 
+		
+		CGRect viewFrame = self.view.frame; 
+		viewFrame.size.height -= keyboardSize.height;
+		
+		scrollView.frame = viewFrame;  
+		keyboardVisible = YES;
+	}
+}
+ 
+- (void) keyboardDidHide: (NSNotification*) notif{
+	
+	if(keyboardVisible)
+	{
+		NSDictionary* info = [notif userInfo];
+		
+		NSValue* aValue = [info objectForKey:UIKeyboardBoundsUserInfoKey];  
+		
+		CGSize keyboardSize = [aValue CGRectValue].size;
+		
+		CGRect viewFrame = self.view.frame; 
+		viewFrame.size.height += keyboardSize.height;
+		
+		scrollView.frame = viewFrame;  
+		keyboardVisible = NO;
+	}
+}
+
+- (IBAction) showAdvancedSettings: (id) sender{
+	
+	if(switchShowAdvanced){
+		
+		txtFieldNumberOfFwd.hidden = YES;
+		txtFieldPointPerGoalFwd.hidden = YES;
+		txtFieldPointPerAssistFwd.hidden = YES;
+		txtFieldPlusMinusFwd.hidden = YES;
+		txtFieldNumberOfDef.hidden = YES;
+		txtFieldPointPerGoalDef.hidden = YES;
+		txtFieldPointPerAssistDef.hidden = YES;
+		txtFieldPlusMinusDef.hidden = YES;
+		txtFieldNumberOfGoalie.hidden = YES;
+		txtFieldPointPerWin.hidden = YES;
+		txtFieldPointPerOT.hidden = YES;
+		txtFieldPointPerShoutout.hidden = YES;
+		txtFieldShoutoutPts.hidden = YES;
+		txtFieldPointPerPts.hidden = YES;
+		txtFieldProlongationPts.hidden = YES;
+		lblNumberOfFwd.hidden = YES;
+		lblPointPerGoalFwd.hidden = YES;
+		lblPointPerAssistFwd.hidden = YES;
+		lblPlusMinusFwd.hidden = YES;
+		lblNumberOfDef.hidden = YES;
+		lblPointPerGoalDef.hidden = YES;
+		lblPointPerAssistDef.hidden = YES;
+		lblPlusMinusDef.hidden = YES;
+		lblNumberOfGoalie.hidden = YES;
+		lblPointPerWin.hidden = YES;
+		lblPointPerOT.hidden = YES;
+		lblPointPerShoutout.hidden = YES;
+		lblShoutoutPts.hidden = YES;
+		lblPointPerPts.hidden = YES;
+		lblProlongationPts.hidden = YES;
+		
+		switchShowAdvanced = NO;
+		
+	}
+	else{
+		
+		txtFieldNumberOfFwd.hidden = NO;
+		txtFieldPointPerGoalFwd.hidden = NO;
+		txtFieldPointPerAssistFwd.hidden = NO;
+		txtFieldPlusMinusFwd.hidden = NO;
+		txtFieldNumberOfDef.hidden = NO;
+		txtFieldPointPerGoalDef.hidden = NO;
+		txtFieldPointPerAssistDef.hidden = NO;
+		txtFieldPlusMinusDef.hidden = NO;
+		txtFieldNumberOfGoalie.hidden = NO;
+		txtFieldPointPerWin.hidden = NO;
+		txtFieldPointPerOT.hidden = NO;
+		txtFieldPointPerShoutout.hidden = NO;
+		txtFieldShoutoutPts.hidden = NO;
+		txtFieldPointPerPts.hidden = NO;
+		txtFieldProlongationPts.hidden = NO;
+		lblNumberOfFwd.hidden = NO;
+		lblPointPerGoalFwd.hidden = NO;
+		lblPointPerAssistFwd.hidden = NO;
+		lblPlusMinusFwd.hidden = NO;
+		lblNumberOfDef.hidden = NO;
+		lblPointPerGoalDef.hidden = NO;
+		lblPointPerAssistDef.hidden = NO;
+		lblPlusMinusDef.hidden = NO;
+		lblNumberOfGoalie.hidden = NO;
+		lblPointPerWin.hidden = NO;
+		lblPointPerOT.hidden = NO;
+		lblPointPerShoutout.hidden = NO;
+		lblShoutoutPts.hidden = NO;
+		lblPointPerPts.hidden = NO;
+		lblProlongationPts.hidden = NO;
+		
+		switchShowAdvanced = YES;
+	}
+	
+}
+
+- (void) initTxtFields{
+	
+	txtFieldPoolName.text = [settings poolName];
+	
+	if([settings numberOfPlayers] != 0)
+		txtFieldNumberOfPlayers.text = [NSString stringWithFormat:@"%d",[settings numberOfPlayers]];
+	if([settings offencePlayerTotal] != 0)
+		txtFieldNumberOfFwd.text = [NSString stringWithFormat:@"%d",[settings offencePlayerTotal]];
+	if([settings offencePointsPerGoal] != 0)
+		txtFieldPointPerGoalFwd.text = [NSString stringWithFormat:@"%d",[settings offencePointsPerGoal]];
+	if([settings offencePointsPerAssist] != 0)
+		txtFieldPointPerAssistFwd.text = [NSString stringWithFormat:@"%d",[settings offencePointsPerAssist]];
+	if([settings offenceDifferentialActivated] != 0)
+		txtFieldPlusMinusFwd.text = [NSString stringWithFormat:@"%d",[settings offenceDifferentialActivated]];
+	if([settings defencePlayerTotal] != 0)
+		txtFieldNumberOfDef.text = [NSString stringWithFormat:@"%d",[settings defencePlayerTotal]];
+	if([settings defencePointsPerGoal] != 0)
+		txtFieldPointPerGoalDef.text = [NSString stringWithFormat:@"%d",[settings defencePointsPerGoal]];
+	if([settings defencePointsPerAssist] != 0)
+		txtFieldPointPerAssistDef.text = [NSString stringWithFormat:@"%d",[settings defencePointsPerAssist]];
+	if([settings defenceDifferentialActivated] != 0)
+		txtFieldPlusMinusDef.text = [NSString stringWithFormat:@"%d",[settings defenceDifferentialActivated]];
+	
 }
 
 
