b97c0a73a33a5b7f9b1dc97598ee96763eb04a4d
alexandre.girard.2 <alexandre.girard.2@0169c884-2b0a-11df-9d00-8333cd69f872>
2010-03-28 18:38:45 +0000

    add pooler list

---
+++ iPool/Classes/PoolerList.h
@@ -0,0 +1,23 @@
+//
+//  PoolerList.h
+//  NavApp
+//
+//  Created by Ian Bussières on 18/03/10.
+//  Copyright 2010 iPool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Foundation/NSString.h>
+#import "Pooler.h"
+
+@interface PoolerList : NSObject {
+        NSMutableArray *poolerList;
+}
+
++ (PoolerList*)getInstance;
+- (NSMutableArray*) poolerList;
+- (void) addPooler: (Pooler*) input;
+
+@property (nonatomic, retain)  NSMutableArray* poolerList;
+
+@end
---
+++ iPool/Classes/PoolerList.m
@@ -0,0 +1,35 @@
+//
+//  PoolerList.m
+//  NavApp
+//
+//  Created by Ian Bussières on 18/03/10.
+//  Copyright 2010 iPool. All rights reserved.
+//
+
+#import "PoolerList.h"
+
+static PoolerList *instance = nil;
+
+
+@implementation PoolerList
+@synthesize poolerList;
+
++ (PoolerList*) getInstance {
+        @synchronized(self) {
+                if (instance == nil) {
+                        instance = [[PoolerList alloc] init];
+                }
+        }
+        return instance;
+}
+
+- (NSMutableArray*) poolerList {
+        return poolerList;
+}
+
+- (void) addPooler: (Pooler*) input
+{
+        [poolerList addObject: input];
+}
+
+@end
